c87c37fbcf46ec5911af4f611a0c23be9fd1a7b6
Manuel-PC <manuel-pc@manzmark-pc>
2025-04-05 11:29:18 +0800

    Changed the bg of the buttons into royalblue

--- src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
@@ -1,6 +1,5 @@
 package aclcbukidnon.com.javafxactivity.controllers;
 
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -9,16 +8,21 @@ public class CounterController {
     @FXML
     private Label labelCount;
 
+    private int count = 0;
 
     @FXML
     protected void onPlusClick() {
-
+        count++;
+        updateLabel();
     }
 
     @FXML
     protected void onMinusClick() {
-
+        count--;
+        updateLabel();
     }
 
-
+    private void updateLabel() {
+        labelCount.setText(String.valueOf(count));
+    }
 }
